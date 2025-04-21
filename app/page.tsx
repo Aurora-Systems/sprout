0838c5c37f7600bace900166a978bbac5c6f93fb
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2025-04-21 14:46:16 +0200

    chore: update package.json to include node-sass and sass dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,23 @@
 import "swiper/css/pagination";
 import BgImg from "./components/bg_img";
 import Image from "next/image";
+import { Fade } from "react-awesome-reveal";
 
 export default function Shop() {
   return (
     <div className="">
       <div className="header d-flex align-items-center justify-content-center" style={{...BgImg("https://ngratesc.sirv.com/sprout/header.png")}}>
-        <div className="text-center">
+        <div className="text-center mb-5">
+          <Fade cascade={true} delay={500}>
+            
           <div>
-          <Image src={"https://ngratesc.sirv.com/sprout/logo.png"}  className="img-fluid mb-5" width={200} height={0} alt="Sprout & Co"/>
+          <Image src={"https://ngratesc.sirv.com/sprout/logo.png"}  className="img-fluid " width={300} height={0} alt="Sprout & Co"/>
           </div>
           <div>
           <button className="btn rounded-pill btn-outline-success">Shop Now</button>
           </div>
+          </Fade>
+
         </div>
       </div>
 
