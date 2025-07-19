bc772b35d3d383839bd9aaa445d028e200e0145d
Zayne <zaynekomichi@hotmail.com>
2025-07-19 16:58:14 +0200

    refactor: remove Supabase client initialization file

--- app/init/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase_url = process.env.SUPABASE_URL as string
-const supabase_key = process.env.SUPABASE_KEY as string
-
-const db = createClient("https://wydajmnykbvjicgnncqz.supabase.co", "d")
-
-export default db
